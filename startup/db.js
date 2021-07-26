2771fc8b650fbb8eef19d72c0c0737da20610c31
olalekan <hammedlukman@gmail.com>
2021-07-26 03:03:13 +0100

    removed error commit

--- startup/db.js
+++ startup/db.js
@@ -6,7 +6,7 @@ module.exports = function () {
     mongoose
       .connect(uri, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,d
+        useUnifiedTopology: true,
       })
       .then(() => console.log("MongoDB connected...."))
       .catch((err) => console.log(err));
